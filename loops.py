44b3dc5ee5fa12ba1c9246c401ef75902fc08504
Sonu Kumar Singh <88200959+error-byNight@users.noreply.github.com>
2023-03-18 18:27:14 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,15 @@
+##While Loop
+a = 1
+while(a<=20):
+    print(a)
+    a += 1
+
+
+
+##for Loop
+for i in range(1,11):
+   ##print(5*i)
+   print("7 X" , str(i) , "=" + str(7*i))
+
+for i in range(1,11):
+    print("500 X" , str(i) , "=" , str(500*i))
