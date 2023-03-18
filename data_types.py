b3d47574e199a191f71cf0776e00b4929ba647ba
Sonu Kumar Singh <88200959+error-byNight@users.noreply.github.com>
2023-03-18 17:58:21 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,19 @@
+##Integer
+a= 100
+print(a)
+print(type(a))
+
+##Float
+b = 100.1
+print(b)
+print(type(b))
+
+##String
+c = "String Data Types"
+print(c)
+print(type(c))
+
+##Boolean
+d = False
+print(d)
+print(type(d))
