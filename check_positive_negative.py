9d2cc20c07c10b0139341b52513bf9155ece40ad
Sonu Kumar Singh <88200959+error-byNight@users.noreply.github.com>
2023-03-23 01:12:20 +0530

    Add files via upload

---
+++ check_positive_negative.py
@@ -0,0 +1,10 @@
+number = float(input("Please the number :"))
+if number < 0 :
+    print("Given nmber", number, "is negative.")
+if number > 0 :
+    print ("Given number", number, "is positive.")
+if number == 0 :
+    print("Given number", number, "is Zero. ")
+    
+    
+
