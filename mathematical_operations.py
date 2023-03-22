d1e1744b6fc9ff2f36616ac657f467791b3b2e51
Sonu Kumar Singh <88200959+error-byNight@users.noreply.github.com>
2023-03-23 01:26:38 +0530

    Add files via upload

---
+++ mathematical_operations.py
@@ -0,0 +1,12 @@
+firstnum = float(input("Enter first number :"))
+secondnum = float(input("Enter second number :"))
+
+add = firstnum + secondnum
+sub = firstnum - secondnum
+mul = firstnum * secondnum
+div = firstnum / secondnum
+
+print("The sum of ",firstnum,"and second number", secondnum,"is", add)
+print("The Difference of",firstnum,"and second number", secondnum,"is", sub)
+print("The multipliation of",firstnum,"and second", secondnum, "is",mul)
+print("The Division of ",firstnum,"and second number" ,secondnum,"is", div)
