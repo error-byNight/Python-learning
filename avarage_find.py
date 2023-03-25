1b9493db2e86d3b31f6a3d9211cd6b69b58194f5
Sonu Kumar Singh <88200959+error-byNight@users.noreply.github.com>
2023-03-25 22:32:50 +0530

    Add files via upload

---
+++ avarage_find.py
@@ -0,0 +1,8 @@
+num_list = input("Enter a list of number separated by space : ").split()
+sum = 0
+for num in num_list:
+    sum += int(num)
+    avg = sum / len(num_list)
+
+print("Sum :", sum)
+print("Average :",avg)
