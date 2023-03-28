11abb2d273f8cb067cd90937591d0f805ad547da
Sonu Kumar Singh <88200959+error-byNight@users.noreply.github.com>
2023-03-29 03:49:23 +0530

    Add files via upload

---
+++ Fibonacci Series.py
@@ -0,0 +1,16 @@
+a, b = 0, 1
+for i in range(10):
+    print(a)
+    a, b = b, a+b
+print("----------------------------")
+print("----------------------------")
+n = int(input("Enter the number of terms you want in the Fibonacci series: "))
+
+# Initializing the first two terms
+a, b = 0, 1
+
+# Print the first n terms of the Fibonacci series
+print("Fibonacci series:")
+for i in range(n):
+    print(a, end='  ,  ')
+    a, b = b, a + b
